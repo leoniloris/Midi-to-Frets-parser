0d829f05ff2a52aa9a1916f4fb665423f460c58b
leoni mota loris <leoniloris1@hotmail.com>
2018-09-01 22:41:21 -1100

    Parse mid files correctly

--- test.py
+++ test.py
@@ -6,56 +6,51 @@ import json
 
 PhraseOwner = Enum('PhraseOwner', 'Team1 Team2 Both', module=__name__)
 
-class Event(Enum):
-
-
 class Note():
-	def __init__(self, start_time_ticks, end_time_ticks, mid_number, previous_event_beat, tempo=500000, ticks_per_beat=480):
+	def __init__(self, start_time_ticks, end_time_ticks, mid_number, tempo=500000, ticks_per_beat=480):
 		'''
 		time_ticks: time in ticks
 		mid_number: note number
 		tempo=500000: microseconds per beat
-		previous_event_beat: beat counter of the last event preceding the current note
 		ticks_per_beat=480: ticks per beat. Provided that `ticks` are the default mid time format
 		'''
 		start_time_in_beats = (start_time_ticks / ticks_per_beat)
 		end_time_in_beats = (end_time_ticks / ticks_per_beat)
 
 		self._mid_number = mid_number
-		self._start_beat = previous_event_beat + start_time_in_beats
-		self._end_beat = self._start_beat + end_time_in_beats
+		self._start_beat = start_time_in_beats
+		self._end_beat = end_time_in_beats
 
 
 class Track():
-	def __init__(self, start_bpm, owner, name='phrase_boss1', tempo=500000, ticks_per_beat=480):
-		self._start_bpm = start_bpm
+	def __init__(self, name='phrase_boss1', tempo=500000, ticks_per_beat=480, owner=None):
 		self._name = name
-		self._owner = owner
+		self._owner = owner # I dont think owner makes sense here.
 	    self._notes = []	    
 
-		self._start_bpm = start_bpm
-		self._start_bpm = start_bpm
-
-	def _dispatch_enclosure(self, notes_needing_enclosure, event):
-		try:
-			notes_needing_enclosure[event.note]
-			return True
-		except Exception as err:
-			print('found note withoud enclosure!', err)
-			return False
-
+	def _dispatch_enclosure(self, notes_starts, event):
+			return notes_starts.pop(event.note, None):
 
 	def parse_midi_events(self, mid_events):
-		notes_needing_enclosure = {}
-
+		notes_starts = {}
+		current_time = 0
 		for event in mid_events:
+			
 			if event.type is 'note_on':
-				notes_needing_enclosure[event.note] = {'start_time': event.time, 'end_time': None}
+				notes_starts[event.note] = event.time + current_time
 
 			if event.type is 'note_off':
-				if self._dispatch_enclosure(notes_needing_enclosure, event):
+				note_start = self._dispatch_enclosure(notes_starts, event)
+				if note is not None:
+					current_time += event.time
+					new_note = Note(note_start, 
+									current_time, 
+									event.note)
+					self._notes.append(new_note)
+				else:
+					print('found note without enclosure!', err)
+					return
 
-				self._notes.append()
 
 def load_mid(file):
 	mid = MidiFile(file)
@@ -73,3 +68,4 @@ def remove_control_msgs(events):
 	return events
 
 def events_to_json(events):
+	raise NotImplementedError()
