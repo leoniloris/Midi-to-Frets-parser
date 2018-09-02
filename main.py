001f2a11aa5d44327c2dbb905749b8f3fd1db306
leoni mota loris <leoniloris1@hotmail.com>
2018-09-02 04:05:40 -1100

    Parse args

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 from midi_operations import *
+import argparse
 import json
 
+def _parse_args():
+    parser = argparse.ArgumentParser(description='Dumps json files from midi files.')
+
+    parser.add_argument(
+        '--file',
+        dest='midi_file',
+        default='test1.midi',
+        help='midi file path')
+    args = parser.parse_args()
+
+    return args
+
 def main():
-	raw_tracks = load_mid('test1.mid')
+	args = _parse_args()
+	raw_tracks = load_mid(args.midi_file)
 	raw_tracks = remove_control_msgs(raw_tracks)
 	tracks = {}
 	for key, track in raw_tracks.items():
 		tracks[key] = Track(name=key)
 		tracks[key].parse_midi_events(track)
 		tracks[key].serialize_notes()
-		# tracks[key] = tracks[key].get_monophonic_notes()
-
-
-
 
 if __name__ == "__main__":
 	main()    
